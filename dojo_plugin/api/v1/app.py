784f4d4e5cd732f2957a5eb701e1fcaa50b22f99
afluffybunny7 <andrewalfredparr@yahoo.com>
2025-03-19 17:21:22 -0400

    More additions

--- dojo_plugin/api/v1/app.py
+++ dojo_plugin/api/v1/app.py
@@ -1,8 +1,8 @@
 from flask_restx import Namespace, Resource
 from pages.app import create_ctf
-app_namespace = Namespace("app", description="Endpoint to manage app")
+appbp_namespace = Namespace("app", description="Endpoint to manage app")
 
-@app_namespace.route('/test-add-page')
+@appbp_namespace.route('/test-add-page')
 class App(Resource):
     def get(self):
         return create_ctf()
--- dojo_plugin/pages/app.py
+++ dojo_plugin/pages/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import Blueprint, Response, render_template, abort
 
-app = Blueprint("pwncollege_app", __name__)
-app.secret_key = 'your_secret_key'
-@app.route('/test-add-page', methods=['GET', 'POST'])
+appbp = Blueprint("pwncollege_app", __name__)
+appbp.secret_key = 'your_secret_key'
+@appbp.route('/test-add-page', methods=['GET', 'POST'])
 def create_ctf():
     return render_template('new_ctf.html')
