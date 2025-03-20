47e565833a4e2cb0454e67efd7f5864086750293
afluffybunny7 <andrewalfredparr@yahoo.com>
2025-03-20 14:04:25 -0400

    Page works at localhost.pwn.college/test-add-page

--- dojo_plugin/api/v1/app.py
+++ dojo_plugin/api/v1/app.py
@@ -1,8 +1,8 @@
 from flask_restx import Namespace, Resource
-from pages.app import create_ctf
+#from pages.app import create_ctf
 app_namespace = Namespace("app", description="Endpoint to manage app")
 
 @app_namespace.route('/test-add-page')
 class App(Resource):
     def get(self):
-        return create_ctf()
+        return {"success"}
